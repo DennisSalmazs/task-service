9c21d1df4c16f5ca9fd73c4924643a3ef9f57e72
DennisSalmazs <skrn.sdet@gmail.com>
2024-09-02 12:58:09 +0100

    project setup

--- src/main/java/com/teambridge/App.java
+++ src/main/java/com/teambridge/App.java
@@ -1,13 +1,13 @@
-package com.teambridge.taskservice;
+package com.teambridge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskServiceApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaskServiceApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
